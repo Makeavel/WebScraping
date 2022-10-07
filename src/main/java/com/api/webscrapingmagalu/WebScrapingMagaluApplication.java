496e6470a68383508135ab5d3acd3390e92c0ce5
Paulo <dpseutroco@gmail.com>
2022-10-07 13:41:47 +0000

    refactor + coleta de dados

--- src/main/java/com/api/webscrapingmagalu/WebScrapingMagaluApplication.java
+++ src/main/java/com/api/webscrapingmagalu/WebScrapingMagaluApplication.java
@@ -11,38 +11,38 @@ public class WebScrapingMagaluApplication {
     public static void main(String[] args) throws IOException, InterruptedException {
 
 
-//            -----------------------  Magazine Luisa  --------------------
+//            -----------------------  Magazine Luiza  --------------------
         MagaluScrapper scrapperTVMG = new MagaluScrapper("https://www.magazineluiza.com.br/busca/smart+tv/?page=");
-        scrapperTVMG.getAllSmartTV(17);
+        scrapperTVMG.getAllProductMagalu(17 , "src/main/java/com/api/webscrapingmagalu/files/MagaluTV.txt");
 
-        MagaluScrapper scrapperSmartphoneMG = new MagaluScrapper("https://www.magazineluiza.com.br/busca/smartphone/?page=");
-        scrapperSmartphoneMG.getAllSmartphone(17);
+       MagaluScrapper scrapperSmartphoneMG = new MagaluScrapper("https://www.magazineluiza.com.br/busca/smartphone/?page=");
+        scrapperSmartphoneMG.getAllProductMagalu(17 , "src/main/java/com/api/webscrapingmagalu/files/MagaluSmartphone.txt");
 
         MagaluScrapper scrapperNotebookMG = new MagaluScrapper("https://www.magazineluiza.com.br/busca/notebook/?page=");
-        scrapperNotebookMG.getAllNotebook(17);
+        scrapperNotebookMG.getAllProductMagalu(17 , "src/main/java/com/api/webscrapingmagalu/files/MagaluNotebook.txt");
 
         MagaluScrapper scrapperWatchMG = new MagaluScrapper("https://www.magazineluiza.com.br/busca/smart+watch/?page=");
-        scrapperWatchMG.getAllSmartwatch(17);
+        scrapperWatchMG.getAllProductMagalu(17 , "src/main/java/com/api/webscrapingmagalu/files/MagaluSmartwatch.txt");
 
         MagaluScrapper scrapperPlacaVideoMG = new MagaluScrapper("https://www.magazineluiza.com.br/busca/placa+de+video/?page=");
-        scrapperPlacaVideoMG.getAllPlacaDeVideo(17);
+        scrapperPlacaVideoMG.getAllProductMagalu(17 , "src/main/java/com/api/webscrapingmagalu/files/MagaluPlacaDevideo.txt");
 
 //              -----------------------  Lojas Americanas  --------------------
 
         AmericanasScrapper scrapperSmartphoneAM = new AmericanasScrapper("https://www.americanas.com.br/categoria/celulares-e-smartphones/smartphone/g/tipo-de-produto-Celular/tipo-de-produto-Iphone/tipo-de-produto-Smartphone?chave=pfm_hm_tt_1_0_smartphone&viewMode=grid&limit=24&offset=");
-        scrapperSmartphoneAM.getAllSmartphoneAM(20);
+        scrapperSmartphoneAM.getAllProductAM(20 , "src/main/java/com/api/webscrapingmagalu/files/AmericanasSmartphone.txt");
 
         AmericanasScrapper scrapperNotebookAM = new AmericanasScrapper("https://www.americanas.com.br/busca/notebook?c_legionRegion=995301&c_macroRegion=MIDWEST_INTERIOR&c_mesoRegion=5301&content=notebook&filter=%7B'id'%3A'categoria'%2C'value'%3A'Informática'%2C'fixed'%3Afalse%7D&sortBy=relevance&source=nanook&testab=searchTestAB%3Dnew&limit=24&offset=");
-        scrapperNotebookAM.getAllNotebookAM(20);
+        scrapperNotebookAM.getAllProductAM(20 , "src/main/java/com/api/webscrapingmagalu/files/AmericanasNotebook.txt");
 
         AmericanasScrapper scrapperSmartwatchAM = new AmericanasScrapper("https://www.americanas.com.br/busca/smartwatch?c_legionRegion=995301&c_macroRegion=MIDWEST_INTERIOR&c_mesoRegion=5301&content=smartwatch&filter=%7B'id'%3A'categoria'%2C'value'%3A'Smartwatch'%2C'fixed'%3Afalse%7D&sortBy=relevance&source=nanook&testab=searchTestAB%3Dnew&limit=24&offset=");
-        scrapperSmartwatchAM.getAllSmartwatchAM(10);
+        scrapperSmartwatchAM.getAllProductAM(10 , "src/main/java/com/api/webscrapingmagalu/files/AmericanasSmartwatch.txt");
 
         AmericanasScrapper scrapperTVAM = new AmericanasScrapper("https://www.americanas.com.br/busca/smart-tv?c_legionRegion=995301&c_macroRegion=MIDWEST_INTERIOR&c_mesoRegion=5301&content=smart%20tv&filter=%7B'id'%3A'categoria'%2C'value'%3A'TV%20e%20Home%20Theater'%2C'fixed'%3Afalse%7D&filter=%7B'id'%3A'categoria'%2C'value'%3A'TV%20e%20Home%20Theater%7CTV'%2C'fixed'%3Afalse%7D&filter=%7B'id'%3A'condicao'%2C'value'%3A'novo'%2C'fixed'%3Afalse%7D&sortBy=relevance&source=nanook&testab=searchTestAB%3Dnew&limit=24&offset=");
-        scrapperTVAM.getAllSmartTVAM(10);
+        scrapperTVAM.getAllProductAM(10 , "src/main/java/com/api/webscrapingmagalu/files/AmericanasTV.txt" );
 
         AmericanasScrapper scrapperPlacaDeVIdeoAM = new AmericanasScrapper("https://www.americanas.com.br/busca/placa-de-video?c_legionRegion=995301&c_macroRegion=MIDWEST_INTERIOR&c_mesoRegion=5301&content=placa%20de%20video&sortBy=relevance&source=nanook&testab=searchTestAB%3Dnew&limit=24&offset=");
-        scrapperPlacaDeVIdeoAM.getAllPlacaDeVideoAM(20);
+        scrapperPlacaDeVIdeoAM.getAllProductAM(20 , "src/main/java/com/api/webscrapingmagalu/files/AmericanasPlacaDeVideo.txt");
 
         // DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         // Date date = new Date();
