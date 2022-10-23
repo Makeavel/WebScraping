edc2af50442ad02b9a67b9dd340d1d5fa9045943
Paulo <dpseutroco@gmail.com>
2022-10-23 16:52:30 +0000

    coleta de dados 23/10

--- src/main/java/com/api/webscrapingmagalu/WebScrapingMagaluApplication.java
+++ src/main/java/com/api/webscrapingmagalu/WebScrapingMagaluApplication.java
@@ -12,7 +12,6 @@ public class WebScrapingMagaluApplication {
 
 
 //              -----------------------  Lojas Americanas  --------------------
-/
         AmericanasScrapper scrapperSmartphoneAM = new AmericanasScrapper("https://www.americanas.com.br/categoria/celulares-e-smartphones/smartphone/g/tipo-de-produto-Celular/tipo-de-produto-Iphone/tipo-de-produto-Smartphone?chave=pfm_hm_tt_1_0_smartphone&viewMode=grid&limit=24&offset=");
         scrapperSmartphoneAM.getAllProductAM(1, 20 , "src/main/java/com/api/webscrapingmagalu/files/AmericanasSmartphone.txt");
 
